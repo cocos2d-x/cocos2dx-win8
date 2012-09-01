099a8acfeb476baba8f9a6fbe789353b7d82f5eb
Denis Mingulov <denis@mingulov.com>
2012-09-01 20:54:05 +0300

    Test is added for the CCDXParticleSystemQuad buffer overflow

---
+++ tests/tests/BugsTest/ParticleBufOverflow.h
@@ -0,0 +1,12 @@
+#ifndef __BUG_PARTICLE_BUFOVERFLOW
+#define __BUG_PARTICLE_BUFOVERFLOW
+
+#include "BugsTest.h"
+
+class BugParticleBufOverflowLayer : public BugsTestBaseLayer
+{
+public:
+    virtual bool init();
+};
+
+#endif
